8e63c7d21ec02952dc981f742b586f3fed46a8bb
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2024-11-21 18:47:24 +0600

    faau

--- js-intro-practice.js
+++ js-intro-practice.js
@@ -1,3 +1,14 @@
-let moneyIHave = 1000;
-let appleAndOrange = 300;
-console.log(moneyIHave - appleAndOrange);
+// let moneyIHave = 1000;
+// let appleAndOrange = 300;
+// console.log(moneyIHave - appleAndOrange);
+
+// let a =20.5,b = 30.500,c = 78,d = 82, e = 72.5000;
+// let avg = (a+b+c+d+e)/5;
+
+// console.log(avg.toFixed(2));
+// ------------------
+//reminder
+let n = 119;
+let divider = 5;
+let rem = n%divider;
+console.log(rem);
