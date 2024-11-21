f3e89f9c5476b4137f743b1402e93d16c4117bac
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2024-11-21 20:21:10 +0600

    ajker jonno

--- loop.js
+++ loop.js
@@ -29,17 +29,56 @@ Write a loop that goes from 1 to 100, but stops (using break) when it encounters
 Write a loop to print even numbers from 1 to 40. Use continue to skip odd numbers.
 */
 
-for(let i = 1;i<=40;i++){
+// for(let i = 1;i<=40;i++){
+//     if(i%2==0){
+//     continue;
+//     console.log(i);
+// }
+// console.log(i);
+// }
+// /*
+// display odd number from 55 to 85 and skip the numbers divisible by 5.
+// */
+// for(let i =55;i<=85;i++){
+//    if(i%2==0 || i%5==0) continue;
+//     console.log(i);
+// }
+
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 91 to 129.
+
+ */
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 51 to 85.
+
+ */
+
+/*programming hero*/
+// let sum = 0
+// for(let i =91;i<=129;i++){
+    
+//     if(i%2!=0){
+//     sum+=i;
+   
+//     }
+    
+// }
+// console.log(sum);
+let sum = 0;
+for(let i =51;i<=85;i++){
+    
     if(i%2==0){
-    continue;
-    console.log(i);
+        console.log(i);
+    sum+=i;
+   
+    }
+    
 }
-console.log(i);
-}
-/*
-display odd number from 55 to 85 and skip the numbers divisible by 5.
-*/
-for(let i =55;i<=85;i++){
-   if(i%2==0 || i%5==0) continue;
-    console.log(i);
-}
+console.log(sum);
