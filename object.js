2ff1233aca2abe032eecf926b2ca34242406dbb7
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2024-11-23 03:55:57 +0600

    ajker jonno js basic till array object rvision ekhanei sesh

--- object.js
+++ object.js
@@ -15,15 +15,15 @@
 // };
 // car.passenger_capacity = '5';
 // console.log(car);
-const student = {
-    name: "Hamim Sakep",
-    id: 5421,
-    physics: {
-        subject: "HSC Physics",
-        author: "Shahjahan Tapan",
-        marks: 30
-    }
-};
-let phy = student.physics
-// let phy = student['physics']
-console.log(phy);
+// const student = {
+//     name: "Hamim Sakep",
+//     id: 5421,
+//     physics: {
+//         subject: "HSC Physics",
+//         author: "Shahjahan Tapan",
+//         marks: 30
+//     }
+// };
+// let phy = student.physics
+// // let phy = student['physics']
+// console.log(phy);
